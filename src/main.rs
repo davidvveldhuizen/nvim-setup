6279b25a2b3c0692ffa1f973ad7649b008b1a7c0
davidvveldhuizen <davidvveldhuizen@gmail.com>
2024-09-30 10:50:56 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+//#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+
+    let mut led = Output::new(p.PA5, Level::High, Speed::Low);
+    let timout = 500;
+    loop {
+        led.toggle();
+        Timer::after_millis(timout).await;
+    }
+}
